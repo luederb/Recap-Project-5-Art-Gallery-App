9a59090fe2121c128a9aa9b3c779ed1d82ec5638
Birk Beese <birk.beese@gmail.com>
2023-10-30 17:03:40 +0100

    start adding spotlight and fixing issues displaing

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,11 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
   const { data } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
+  console.log(data);
+
+  function getRandomInt() {
+    return Math.floor(Math.random() * 11);
+  }
   return (
     <>
       <GlobalStyle />
